928b76c0ad0241a126323a57b4f7d3f3292a8b5b
Uttam2007110 <120580951+Uttam2007110@users.noreply.github.com>
2023-07-06 19:52:03 +0530

    quality of life upgrades

--- concat.py
+++ concat.py
@@ -5,43 +5,42 @@ Concate single match data into 1 file
 @author: Subramanya.Ganti
 """
 
-# importing the required modules
 import glob
 import pandas as pd
 from datetime import datetime
  
-# specifying the path to csv files
-path = "C:/Users/Subramanya.Ganti/Downloads/cricket/blast"
-output = 'blast.csv'
+comp = 'blast'
+path = f"C:/Users/Subramanya.Ganti/Downloads/cricket/{comp}"
+output = f'{comp}.csv'
 db_start = 2017
+league = 0; check = 0
  
-# csv files in the path
 file_list = glob.glob(path + "/*.csv")
- 
-# list of excel files we want to merge.
-# pd.read_excel(file_path) reads the 
-# excel data into pandas dataframe.
 excl_list = []
-countries = ["Afghanistan","Australia","Bangladesh","England","India","Ireland","Namibia","Netherlands","New Zealand","Pakistan","Scotland","South Africa","Sri Lanka","United Arab Emirates","West Indies","Zimbabwe"]
- 
+if(comp == 'odiq'):
+    countries = ["Zimbabwe","Netherlands","West Indies","Nepal","United States of America","Sri Lanka","Ireland","Scotland","Oman","United Arab Emirates"]
+elif(comp == 't20w'):
+    countries = ["Australia","England","New Zeland","India","South Africa","Sri Lanka","Ireland","Scotland","West Indies","Pakistan","Bangladesh","Thailand","Zimbabwe"]
+elif(comp == 'odiw'):
+    countries = ["Australia","England","New Zeland","India","South Africa","Sri Lanka","West Indies","Pakistan","Bangladesh","Thailand"]
+elif(comp == 'odi'): 
+    countries = ["Australia","England","New Zeland","India","South Africa","Sri Lanka","Pakistan","Bangladesh","Afghanistan","West Indies"]
+elif(comp == 't20i'):
+    countries = ["Australia","England","New Zeland","India","South Africa","Sri Lanka","West Indies","Pakistan","Bangladesh","Ireland","Afghanistan","Zimbabwe","Scotland","Netherlands","Namibia","United Arab Emirates"]
+else:
+    league = 1
+
 for file in file_list:
     if(file[-8:] != "info.csv"):
         df = pd.read_csv(file)
-        date = datetime.strptime(df['start_date'][0], '%Y-%m-%d')        
-        #if(df['batting_team'][0] in countries and df['bowling_team'][0] in countries):
-        if(date.year>=db_start):
+        date = datetime.strptime(df['start_date'][0], '%Y-%m-%d')
+        if(league == 0):check = df['batting_team'][0] in countries and df['bowling_team'][0] in countries
+        if(date.year>=db_start and (check or league)):
             df['season'] = date.year
             print(df['start_date'][0],df['batting_team'][0],df['bowling_team'][0])
             excl_list.append(df)
  
 print("data collected, concat starts")
-# concatenate all DataFrames in the list
-# into a single DataFrame, returns new
-# DataFrame.
 excl_merged = pd.concat(excl_list, ignore_index=True)
-# exports the dataframe into excel file
-# with specified name.
-print("concat done, file dumped")
-
 excl_merged.to_csv(output, index=False)
-
+print("concat done, file dumped")
