4d861792e70841f0321ff8a2e177e00bb06a2345
Uttam2007110 <120580951+Uttam2007110@users.noreply.github.com>
2024-12-18 17:38:00 +0530

    smat teams exclusion list updated

--- core/concat.py
+++ core/concat.py
@@ -9,11 +9,13 @@ import glob
 import pandas as pd
 from datetime import datetime
  
-comp = 'blast'
+comp = 'smat'
 if(comp == 'odiq'):
     path = "C:/Users/Subramanya.Ganti/Downloads/cricket/raw/odi"
 elif(comp == 't20iq'):
     path = "C:/Users/Subramanya.Ganti/Downloads/cricket/raw/t20i"
+elif(comp == 't20iwq'):
+    path = "C:/Users/Subramanya.Ganti/Downloads/cricket/raw/t20iw"
 else:
     path = f"C:/Users/Subramanya.Ganti/Downloads/cricket/raw/{comp}"
 output = f'C:/Users/Subramanya.Ganti/Downloads/cricket/raw/{comp}.csv'
@@ -29,18 +31,22 @@ if(comp == 'odiq'):
     countries = ["Zimbabwe","Netherlands","West Indies","Nepal","United States of America","Sri Lanka","Ireland","Scotland","Oman","United Arab Emirates"]
 elif(comp == 't20iw'):
     countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","Ireland","Scotland","West Indies","Pakistan","Bangladesh","Thailand","Zimbabwe"]
+elif(comp == 't20iwq'):
+    countries = ["Ireland","Scotland","Thailand","Zimbabwe","Papua New Guinea","Netherlands","Namibia","United Arab Emirates","Uganda","Tanzania","Indonesia","Nepal"]
 elif(comp == 'odiw'):
     countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","West Indies","Pakistan","Bangladesh","Thailand","Ireland"]
 elif(comp == 'odi'):
-    countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","Pakistan","Bangladesh","Afghanistan","West Indies","Netherlands","Nepal","Ireland"]
+    countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","Pakistan","Bangladesh","Afghanistan","West Indies","Netherlands","Nepal","Ireland","Zimbabwe"]
 elif(comp == 't20i'):
     countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","West Indies","Pakistan","Bangladesh","Ireland","Afghanistan","Zimbabwe","Scotland","Netherlands","Namibia","United Arab Emirates","Nepal","Oman","Papua New Guinea","Canada","United States of America","Uganda"]
 elif(comp == 't20iq'):
     countries = ["Ireland","Scotland","Jersey","Italy","Germany","Denmark","Austria","Netherlands","Zimbabwe","Namibia","Nigeria","Rwanda","Tanzania","Uganda","Kenya","Bahrain","Hong Kong","Kuwait","Malaysia","Nepal","Oman","Singapore","United Arab Emirates","Japan","Papua New Guinea","Philippines","Canada","United States of America"]
 elif(comp == 'tests'):
-    countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","Pakistan","West Indies"]
+    countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","Pakistan","West Indies","Bangladesh"]
 elif(comp == 'cwc'):
     countries = ["Australia","England","New Zealand","India","South Africa","Sri Lanka","Pakistan","Bangladesh","Afghanistan","West Indies","Netherlands","Nepal","Ireland","Zimbabwe","Kenya","Namibia"]
+elif(comp == 'smat'):
+    countries = ['Meghalaya','Mizoram','Sikkim','Arunachal Pradesh','Manipur','Nagaland','Pondicherry']
 else:
     league = 1
 
@@ -49,9 +55,12 @@ for file in file_list:
         df = pd.read_csv(file)
         date = datetime.strptime(df['start_date'][0], '%Y-%m-%d')
         if(league == 0):check = df['batting_team'][0] in countries and df['bowling_team'][0] in countries
+        if(comp == 'smat'):check = df['batting_team'][0] not in countries and df['bowling_team'][0] not in countries
         if(date.year>=db_start and (check or league) and date.year<=db_end):
             if((comp == 'bbl' or comp == 'wbbl' or comp == 'ss' or comp == 'wss' or comp == 'shield') and date.month < 4):   #big bash is a december-january league
                 df['season'] = date.year-1
+            elif(comp == 'smat' and date.year>=db_start and date.year<=db_end):
+                df['season'] = int((df['season'][0]).split('/')[0])
             else:                
                 df['season'] = date.year
             print(df['start_date'][0],df['batting_team'][0],df['bowling_team'][0])
@@ -63,12 +72,15 @@ for file in file_list:
         t1 = df.loc[(df['col2']=='team') | (df['col2']=='teams'),'team'].values[0]
         t2 = df.loc[(df['col2']=='team') | (df['col2']=='teams'),'team'].values[1]
         if(league == 0):check = t1 in countries and t2 in countries
+        if(comp == 'smat'):check = t1 not in countries and t2 not in countries
         if(date.year>=db_start and (check or league) and date.year<=db_end):
             names = df.loc[(df['col2']=='player') | (df['col2']=='players'),'player']
             names = pd.concat([names,pd.Series([t1,t2])])
             names = pd.DataFrame(names)
             if((comp == 'bbl' or comp == 'wbbl' or comp == 'ss' or comp == 'wss' or comp == 'shield') and date.month < 4):   #big bash is a december-january league
                 names['season'] = date.year-1
+            elif(comp == 'smat' and date.year>=db_start and date.year<=db_end):
+                names['season'] = int((df.loc[df['col2']=='season','team'].values[0]).split('/')[0])
             else:                
                 names['season'] = date.year
             names = names.rename(columns={0: 'player'})
